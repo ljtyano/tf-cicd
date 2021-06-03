84054de8025edc35cd92fae2e195ee11605ad712
ljtyano <84890700+ljtyano@users.noreply.github.com>
2021-06-03 19:44:29 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
     resource_group_name = "tfrmRG"
     storage_account_name = "tfrm"
     container_name = "tfrm-container"
-	sas_token = "?sv=2020-02-10&ss=bfqt&srt=co&sp=rwdlacuptfx&se=2021-06-03T13:57:31Z&st=2021-06-03T05:57:31Z&spr=https&sig=18Rn0t8p3G0tH2%2BZ%2F1tX2WfvTYYLn07lLECIijYYR2c%3D" 
+    sas_token = "?sv=2020-02-10&ss=bfqt&srt=co&sp=rwdlacuptfx&se=2021-06-03T13:57:31Z&st=2021-06-03T05:57:31Z&spr=https&sig=18Rn0t8p3G0tH2%2BZ%2F1tX2WfvTYYLn07lLECIijYYR2c%3D" 
   }
 }
 
